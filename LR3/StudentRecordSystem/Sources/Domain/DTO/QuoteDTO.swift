483a830dba4aa5a41fe52dff2a0fa1cb3d055e07
Artiom <artiom.sin@gmail.com>
2025-05-05 17:36:07 +0300

    Add StudentDTO and QuoteDTO for data transfer

--- LR3/StudentRecordSystem/Sources/Domain/DTO/QuoteDTO.swift
+++ LR3/StudentRecordSystem/Sources/Domain/DTO/QuoteDTO.swift
@@ -1,1 +1,8 @@
+import Foundation
+
+struct QuoteDTO: Codable {
+    let q: String   // Цитата
+    let a: String   // Автор
+}
+
 
--- LR3/StudentRecordSystem/Sources/Domain/DTO/StudentDTO.swift
+++ LR3/StudentRecordSystem/Sources/Domain/DTO/StudentDTO.swift
@@ -1,1 +1,10 @@
+import Foundation
+
+struct StudentDTO: Codable {
+    let id: String
+    let name: String
+    let age: Int
+    let grade: Int
+    let quotes: [QuoteDTO]
+}
 
