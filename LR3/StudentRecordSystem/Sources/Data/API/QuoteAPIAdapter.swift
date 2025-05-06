d36d95389dde972af3782a5cb21f76e3055af5bd
Artiom <artiom.sin@gmail.com>
2025-05-06 09:44:50 +0300

    Implement StudentRepository with JSON file storage

--- LR3/StudentRecordSystem/Sources/Data/API/QuoteAPIAdapter.swift
+++ LR3/StudentRecordSystem/Sources/Data/API/QuoteAPIAdapter.swift
@@ -1,1 +1,41 @@
+import Foundation
+
+class QuoteAPIAdapter {
+    private let apiURL = URL(string: "https://zenquotes.io/api/random")!
+
+    func fetchRandomQuote(completion: @Sendable @escaping (QuoteDTO?) -> Void) {
+        let task = URLSession.shared.dataTask(with: apiURL) { data, response, error in
+            if let error = error {
+                print("Ошибка при вызове API: \(error.localizedDescription)")
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+
+            guard let data = data else {
+                print("Данные отсутствуют.")
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+
+            do {
+                let decoder = JSONDecoder()
+                let quotes = try decoder.decode([QuoteDTO].self, from: data)
+                DispatchQueue.main.async {
+                    completion(quotes.first)
+                }
+            } catch {
+                print("Ошибка парсинга JSON: \(error)")
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+            }
+        }
+
+        task.resume()
+    }
+}
 
--- LR3/StudentRecordSystem/Sources/Data/Repository/StudentRepository.swift
+++ LR3/StudentRecordSystem/Sources/Data/Repository/StudentRepository.swift
@@ -1,1 +1,26 @@
+import Foundation
+
+class StudentRepository {
+    private let fileName = "student_data.json"
+    private var fileURL: URL {
+        FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(fileName)
+    }
+    
+    func loadStudents() throws -> [Student] {
+        guard FileManager.default.fileExists(atPath: fileURL.path) else {
+            return []
+        }
+        
+        let data = try Data(contentsOf: fileURL)
+        let dtos = try JSONDecoder().decode([StudentDTO].self, from: data)
+        return try dtos.map { try StudentFactory.create(from: $0) }
+    }
+    
+    func saveStudents(_ students: [Student]) throws {
+        let dtos = students.map { StudentFactory.createDTO(from: $0) }
+        let data = try JSONEncoder().encode(dtos)
+        try data.write(to: fileURL)
+    }
+}
+
 
